3df0d7c0781153cacb5b278a6cf106c9259dd160
hakre <hanskrentel@yahoo.de>
2012-09-09 12:35:42 +0200

    Added file to test if code "compiles" clean.

---
+++ compile.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * DCI Account Example in PHP
+ *
+ * "Compile" script
+ *
+ * @author hakre
+ */
+
+require __DIR__ . '/src/autoloader.php';
+
+/// Methodless Role Types ///
+
+abstract class CMoneySource implements MoneySource
+{
+}
+
+class CMoneySink extends CMoneySource implements MoneySink
+{
+    function decreaseBalance(\App\Currency $amount) {
+    }
+
+    function increaseBalance(\App\Currency $amount) {
+    }
+
+    function updateLog($string, \DateTime $time, \App\Currency $currency) {
+    }
+
+    function transferFrom(\App\Currency $amount, MoneySource $source) {
+    }
+
+    function transferTo(\App\Currency $amount, MoneySink $recipient) {
+    }
+}
+
+new CMoneySink();
+
+/// Currency ///
+
+class CCurrency extends \App\Currency
+{
+}
