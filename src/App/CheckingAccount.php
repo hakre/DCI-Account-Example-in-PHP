ae33780604d2fb0db55c8fb7ba824359e6f58fd1
hakre <hanskrentel@yahoo.de>
2012-09-11 13:13:53 +0200

    Added Account types.

---
+++ src/App/CheckingAccount.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * DCI Account Example in PHP
+ *
+ * CheckingAccount
+ *
+ * @author hakre
+ */
+
+namespace App;
+
+class CheckingAccount extends Account
+{
+    protected $toString = 'Checking';
+}
---
+++ src/App/SavingsAccount.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * DCI Account Example in PHP
+ *
+ * SavingsAccount
+ *
+ * @author hakre
+ */
+
+namespace App;
+
+class SavingsAccount extends Account
+{
+    protected $toString = 'Savings';
+}
