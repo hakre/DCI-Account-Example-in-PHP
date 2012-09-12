8beb96e0db4e516144138250721ca58ca5dc278f
hakre <hanskrentel@yahoo.de>
2012-09-12 15:35:12 +0200

    Update app example for demo of Casting.

--- example/app.php
+++ example/app.php
@@ -13,3 +13,24 @@ header("Content-Type: text/plain");
 
 print "DCI Account Example in PHP\n";
 print "==========================\n";
+
+Use DCI\Casting;
+Use App\SavingsAccount;
+Use App\CheckingAccount;
+Use App\Currency;
+
+
+$casting = new Casting();
+
+$source = new SavingsAccount;
+$casting->setRoleAndScript('MoneySource', 'TransferMoneySource')->castOf($source);
+
+$sink = new CheckingAccount;
+$casting->setRoleAndScript('MoneySink', 'TransferMoneySink')->castOf($sink);
+
+$source->increaseBalance(new Currency(100000));
+$source->transferTo(new Currency(200), $sink);
+
+printf("Source: %s: Sink: %s\n", $source->getAvailableBalance(), $sink->getAvailableBalance());
+
+
